b73bebff1a8f96cabe3b9d307f77c16df246e2fd
Christine Ting <christ123092@yahoo.com>
2020-02-07 11:18:16 -0800

    revised schema and seeding data for 100 products

--- product rec module/database/schema.js
+++ product rec module/database/schema.js
@@ -1,13 +1,8 @@
 var mongoose = require('mongoose');
 
 var PRSchema = new mongoose.Schema({
-  number: { type: Number, unique: true, required: true, dropDups: true },
-  image: { type: String, required: true},
-  rating: { type: Number, required: true },
-  numReviews: { type: Number, required: true },
-  brand: { type: String, required: true },
-  name: { type: String, required: true },
-  price: { type: String, required: true },
+  productID: { type: Number, unique: true, required: true, dropDups: true },
+  productName: { type: String, required: true},
   desc: { type: String },
   features: [{ type: String }],
   specs: {
@@ -23,7 +18,16 @@ var PRSchema = new mongoose.Schema({
     weight: [{ type: String }]
   },
   notes: String,
-  view: String
+  view: String,
+  list: [{
+    number: { type: Number, unique: true, required: true, dropDups: true },
+    image: { type: String, required: true},
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true },
+    brand: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: String, required: true },
+  }]
 });
 
 module.exports = PRSchema;
--- product rec module/database/seed.js
+++ product rec module/database/seed.js
@@ -2,59 +2,72 @@ const mongoose = require('mongoose');
 const Product = require('./'); // ./ defaults to index
 var imageDetails = require('./imageDetails.json');
 const faker = require('faker');
-let products = [];
+let productRecList = [];
 
 
-// produce 100 product objects, then push each into products array
+// produce 100 products to put in product recommendations list
 for (var i = 0; i < 100; i++) {
-  let product = {};
-  product.number = i;
-  product.image = imageDetails[i].link;
-  product.rating = Math.floor(Math.random() * 100);
-  product.numReviews = Math.floor(Math.random() * 200);
-  product.brand = imageDetails[i].brand;
-  product.name = imageDetails[i].title;
-  product.price = Math.floor(Math.random() * 500) + 0.95;
-  if (imageDetails[i].desc && imageDetails[i].features && imageDetails[i].specs && imageDetails[i].view) {
+  let productRec = {
+    number: i,
+    image: imageDetails[i].link,
+    rating: Math.floor(Math.random() * 100),
+    numReviews: Math.floor(Math.random() * 200),
+    brand: imageDetails[i].brand,
+    name: imageDetails[i].title,
+    price: Math.floor(Math.random() * 500) + 0.95
+  };
+  if (productRec.numReviews === 0) {
+    productRec.rating = 0;
+  }
+  productRecList.push(productRec);
+}
+/* produce 100 products with product details and randomly select 12 different products from product recommendations list 
+ and store them as an array in key 'list' under each product, then seed each into database */
+for (var i = 0; i < 100; i++) {
+  let product = {
+    productID: i,
+    productName: productRecList[i].name,
+    list: []
+  };
+
+  // if part of the product details exist in imageDetails file, get it and store in product details
+  if (imageDetails[i].desc && imageDetails[i].features && imageDetails[i].specs && imageDetails[i].view && imageDetails[i].notes) {
     product.desc = imageDetails[i].desc;
     product.features = imageDetails[i].features;
     product.specs = imageDetails[i].specs;
+    product.notes = imageDetails[i].notes;
     product.view = imageDetails[i].view;
   } else {
+  // otherwise, randomly generate data
     product.desc = faker.hacker.phrase() + ' ' + faker.lorem.sentences(1);
     product.features = [];
     let numFeature = Math.floor(Math.random() * 15);
-    for (var j = 0; j < numFeature.length; j++) {
+    for (var j = 0; j < numFeature; j++) {
       let feature = faker.hacker.phrase() + ' ' + faker.lorem.sentences(faker.random.number({ min: 1, max: 3 }));
       product.features.push(feature);
     }
     product.specs = {
-      "Best Use": faker.hacker.ingverb(),
-      material: faker.hacker.noun(),
+      bestUse: faker.hacker.ingverb(),
+      materials: faker.hacker.noun(),
       dimensions: `${faker.random.number({ min: 1, max: 20 })} x ${faker.random.number({ min: 1, max: 20 })} x ${faker.random.number({ min: 1, max: 20 })} inches`,
       weight: `${faker.random.number({ min: 2, max: 20 })} ounces`
     }
-    product.view = `View all ${product.brand} products`;
+    product.view = `View all related products`;
   }
-  if (product.numReviews === 0) {
-    product.rating = 0;
+  // randomly select 12 products from productRecList to store in the key 'productRecList'
+  let indexContainer = [];
+  for (var k = 0; k < 12; k++) {
+    let randomIndex = Math.floor(Math.random() * 100);
+    if (!indexContainer.includes(randomIndex) && productRecList[randomIndex].name !== product.productName) {
+      indexContainer.push(randomIndex);
+      product.list.push(productRecList[randomIndex]);
+    }
   }
-  products.push(product);
-}
-
-
-// seed the data with 12 products
-var seedData = () => {
-  Product.create(products.slice(0, 12))
+  Product.create(product)
     .then(() => {
       console.log('database seeded');
       mongoose.connection.close();
     })
     .catch(err => console.error(err));
-};
-
-seedData();
-// NOTE: DO NOT invoke this function as part of your
-// server code - it is meant to only be run once so that
-// you have access to data to work with
+}
 
