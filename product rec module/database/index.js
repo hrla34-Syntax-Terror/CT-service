2cbf8990bef6a783b38cba39c093755378db477b
Christine Ting <christ123092@yahoo.com>
2020-03-21 21:05:27 -0700

    minor fixes

--- product rec module/database/index.js
+++ product rec module/database/index.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 const PRSchema = require('./schema.js');
-mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost/PRdata', {useNewUrlParser: true})
   .then(() => console.log('db connected'));
 
-  const Product = mongoose.model('Product', PRSchema); 
+const Product = mongoose.model('Product', PRSchema); 
 
 module.exports = Product;
